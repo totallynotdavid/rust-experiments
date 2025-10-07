2c9a68a14cf055ef39d921044e9a18aab22fa93c
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-10-07 11:44:04 -0500

    solved exercise 04: primitive types

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -3,6 +3,9 @@ fn main() {
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
+    let (name, age) = cat;
 
+    // so, this works, but was not really the task
+    // println!("{name} is {age} years old", name = cat.0, age = cat.1);
     println!("{name} is {age} years old");
 }
